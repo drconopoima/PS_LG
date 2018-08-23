9beb4ad6419fe43426e158379a0c89ce83b8d130
Luis Diaz <luis@qualiaanalytics.org>
2018-08-23 09:45:46 +0200

    Manejo de errores para la funcion factors(n) con mensaje informativo respecto al tipo de argumento

--- ejercicio1/perfect_num_class.py
+++ ejercicio1/perfect_num_class.py
@@ -2,6 +2,15 @@
 from functools import reduce
 from math import sqrt
 
+class NotIntOrLong(TypeError):
+    """
+    Español:
+     * [NotIntOrLong(TypeError) Clase de error personalizada para proporcionar una descripción de error ligeramente más informativa cuando los argumentos para los factores no son de tipo int o long]
+    English:
+     * [NotIntOrLong(TypeError) Custom Error class for providing slightly more informative error description when the arguments for factors aren't of type int or long]
+    """
+    pass
+
 def factoring_list(list):
     """
      * [factoring_list(list) finds all of the factors of a list containing integers and returns them within a list that contains several lists of factors]
@@ -21,7 +30,7 @@ def factors(n):
      * @param  {[int o long]} n [un número entero al que quiere encontrarsele factores]
      * @return {[set]}        result_factors [una lista de factores para el número entero]
 
-     Basado en las respuestas de Stack Overflow por agf y Steinar Lima con licencia cc by-sa 3.0 con atribución requerida.
+     Basado en respuestas de Stack Overflow por agf y Steinar Lima con licencia cc by-sa 3.0 con atribución requerida.
 
     ENGLISH:
      * [factors(n) finds all the factors of an integer]
@@ -30,11 +39,12 @@ def factors(n):
 
      Based on Stack Overflow answers by agf and Steinar Lima licensed cc by-sa 3.0 with attribution required.
     """
-    step = 2 if n%2 else 1
-    return set(reduce(list.__add__,
-                ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
-
-
+    try:
+        step = 2 if n%2 else 1
+        return set(reduce(list.__add__,
+                    ([i, n//i] for i in range(1, int(sqrt(n))+1, step) if n % i == 0)))
+    except TypeError:
+        raise NotIntOrLong("Please provide an Integer or a Long as arguments to function factors")
 
 def perfect_calculation(list):
     pass
