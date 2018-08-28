3bd38d2500bd3fbc1c2f952132ace24f74717b51
Luis Diaz <luis@qualiaanalytics.org>
2018-08-28 02:02:03 +0200

    parseados, consolidados y sumados los datasets 1 y 2

--- ejercicio2/src/App.js
+++ ejercicio2/src/App.js
@@ -3,14 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Highcharts from 'highcharts';
 import {
-  HighchartsChart, Chart, withHighcharts, XAxis, YAxis, Title, Subtitle, Legend, LineSeries, SplineSeries
+  HighchartsChart, Chart, withHighcharts, XAxis, YAxis, Title, Subtitle, Legend, LineSeries
 } from 'react-jsx-highcharts';
 import _ from 'lodash';
 import 'tachyons';
 
+let dataTotals2 = {
+  'CAT 1': [],
+  'CAT 2': [],
+  'CAT 3': [],
+  'CAT 4': []
+}
+
 const urls = [
   'https://s3.amazonaws.com/logtrust-static/test/test/data1.json',
-  'https://s3.amazonaws.com/logtrust-static/test/test/data3.json',
+  'https://s3.amazonaws.com/logtrust-static/test/test/data2.json',
   'https://s3.amazonaws.com/logtrust-static/test/test/data3.json'
 ]
 
@@ -51,37 +58,121 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      data1: {
-        d: [],
-        value: [],
-        cat: []
-      },
-      data2: {
-        myDate: [],
-        categ: [],
-        val: []
-      },
-      data3: {
-        raw: [],
-        val: [],
+      rawData: [],
+      data1: {},
+      data2: {},
+      data3: {},
+      consolidatedData: true,
+      dataTotals: {},
+      /*  date: {
+          'CAT 1': 0,
+          'CAT 2': 0,
+          'CAT 3': 0,
+          'CAT 4': 0
+        }
+        dates: []
       }
+      */
+      categoryTotals: {},
+      /*
+        'CAT 1': 0,
+        'CAT 2': 0,
+        'CAT 3': 0,
+        'CAT 4': 0,
+      }
+      */
     }
   }
   componentDidMount() {
-    fetch(urls[0])
-      .then(response => response.json())
-      .then(firstdataset => {this.setState({data1: firstdataset})});
-    fetch(urls[1])
-      .then(response => response.json())
-      .then(seconddataset => {this.setState({data2: seconddataset})});
-    fetch(urls[2])
-      .then(response => response.json())
-      .then(thirddataset => {this.setState({data3: thirddataset})});
+    const promise1 = fetch(urls[0]).then(response => response.json())
 
+    promise1.then(firstdataset => this.setState({data1: firstdataset}));
+
+    const promise2 = fetch(urls[1]).then(response => response.json())
+
+    promise2.then(seconddataset => this.setState({data2: seconddataset}));
+
+    const promise3 = fetch(urls[2]).then(response => response.json())
+
+    promise3.then(thirddataset => this.setState({data3: thirddataset}));
+
+    Promise.all([promise1, promise2, promise3])
+      .then(allResponses => this.setState({rawData: allResponses}))
+  }
+  componentDidUpdate() {
+    if (this.state.consolidatedData) {
+      const {data1, data2, data3} = this.state
+      let {dataTotals, categoryTotals} = this.state
+      dataTotals.dates = [];
+      if (data1.length && data2.length && data3.length) {
+        let whichCAT
+        let value
+        let whichDate
+        this.state.consolidatedDate = false; // no consolidar la data dos veces en subsiguientes updates
+        categoryTotals = {
+          'CAT 1': 0,
+          'CAT 2': 0,
+          'CAT 3': 0,
+          'CAT 4': 0
+        }
+        for (let elementData1 of data1) {
+          whichCAT = elementData1.cat.toUpperCase();
+          whichDate = this.formatDate(new Date(elementData1.d));
+          value = Number(elementData1.value);
+          if (whichDate in dataTotals) {
+            if (whichCAT in dataTotals[whichDate]) {
+              dataTotals[whichDate][whichCAT] += value;
+              categoryTotals[whichCAT] += value;
+            } else {
+              dataTotals[whichDate][whichCAT] = 0;
+              dataTotals[whichDate][whichCAT] += value;
+              categoryTotals[whichCAT] += value;
+            }
+          } else {
+            dataTotals.dates = [...dataTotals.dates,whichDate];
+            dataTotals[whichDate] = {};
+            dataTotals[whichDate][whichCAT] = 0;
+            dataTotals[whichDate][whichCAT] += value;
+            categoryTotals[whichCAT] += value;
+          }
+        }
+        for (let elementData2 of data2) {
+          let whichCAT
+          let value
+          let whichDate
+          whichCAT = elementData2.categ.toUpperCase();
+          value = Number(elementData2.val);
+          whichDate = this.formatDate(new Date(elementData2.myDate));
+          if (whichDate in dataTotals) {
+            if (whichCAT in dataTotals[whichDate]) {
+              dataTotals[whichDate][whichCAT] += value;
+              categoryTotals[whichCAT] += value;
+            } else {
+              dataTotals[whichDate][whichCAT] = value;
+              categoryTotals[whichCAT] += value;
+            }
+          } else {
+            dataTotals[whichDate] = {};
+            dataTotals[whichDate][whichCAT] = value;
+            categoryTotals[whichCAT] += value;
+          }
+        }
+        this.state.dataTotals = dataTotals;
+        this.state.categoryTotals = categoryTotals;
+      }
+    }
   }
   render() {
-    const { data1, data2, data3 } = this.state;
-    return !data1.length ?
+    if (!(_.isEmpty(this.state.categoryTotals) && _.isEmpty(this.state.dataTotals))) {
+      for(let i=0; i<this.state.dataTotals.dates.length; i++) {
+        dataTotals2['CAT 1'][i] = [this.state.dataTotals.dates[i], this.state.dataTotals[this.state.dataTotals.dates[i]]['CAT 1']];
+        dataTotals2['CAT 2'][i] = [this.state.dataTotals.dates[i], this.state.dataTotals[this.state.dataTotals.dates[i]]['CAT 2']];
+        dataTotals2['CAT 3'][i] = [this.state.dataTotals.dates[i], this.state.dataTotals[this.state.dataTotals.dates[i]]['CAT 3']];
+        dataTotals2['CAT 4'][i] = [this.state.dataTotals.dates[i], this.state.dataTotals[this.state.dataTotals.dates[i]]['CAT 4']];
+      }
+    }
+    console.log(dataTotals2['CAT 1'][0])
+    return (_.isEmpty((this.state.categoryTotals)) && _.isEmpty(this.state.dataTotals)) ?
       <h1>Loading</h1> :
     (
       <div className="App">
@@ -92,7 +183,7 @@ class App extends Component {
               <Chart />
 
               <Title>Ejercicio 2</Title>
-              <Subtitle>Dataset 1</Subtitle>
+              <Subtitle>Dataset 1 y 2 consolidados juntos</Subtitle>
               <Legend layout="vertical" align="right" verticalAlign="middle" />
 
               <XAxis type="datetime">
@@ -101,27 +192,25 @@ class App extends Component {
 
               <YAxis>
                 <YAxis.Title>Value</YAxis.Title>
-                <LineSeries name="Cat 1" data={this.state.data1.map(data => {
-                  return data.cat==="Cat 1"? [data.d, data.value] : false;
-                }).filter(data => data)} />
-                <LineSeries name="Cat 2" data={this.state.data1.map(data => {
-                  return data.cat==="Cat 2"? [data.d, data.value] : false;
-                }).filter(data => data)} />
-                <LineSeries name="Cat 3" data={this.state.data1.map(data => {
-                  return data.cat==="Cat 3"? [data.d, data.value] : false;
-                }).filter(data => data)} />
-                <LineSeries name="Cat 4" data={this.state.data1.map(data => {
-                  return data.cat==="Cat 4"? [data.d, data.value] : false;
-                }).filter(data => data)} />
+                      <LineSeries name="CAT 1" data={dataTotals2['CAT 1']} />
+                      <LineSeries name="CAT 2" data={dataTotals2['CAT 2']} />
+                      <LineSeries name="CAT 3" data={dataTotals2['CAT 3']} />
+                      <LineSeries name="CAT 4" data={dataTotals2['CAT 4']} />
               </YAxis>
 
             </HighchartsChart>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         </div>
-    );
+    )
+  }
+  formatDate(d) {
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    let year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
   }
 }
 
